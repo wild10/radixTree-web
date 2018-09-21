41e2f88f19fdaebfd9f635681caf34f170b3021d
wilderd <wildr.10@gmail.com>
2018-09-21 13:23:14 -0500

    first commit2

---
+++ js/controlIndex.js
@@ -0,0 +1,160 @@
+
+
+var c = document.getElementById("miCanvas");
+var ctx = c.getContext("2d");
+// graficar el circulo
+ctx.beginPath();
+var n = 0;
+var array = [];
+
+function insertData(){
+
+  // variables globales
+  var h_center,k_center, str ;
+  // root center(h,k)
+  h_center = 250;
+  k_center = 75;
+
+  var str = document.getElementById("txtInsert").value;  // para tamaño de letra <5
+  var bool = -1;
+
+  console.log("length: "+ array.length);
+  array[array.length] = str;
+
+  // para los hijos
+
+  for(var i = 0; i < array.length; i++){
+      if(i === 0){
+
+        ctx.fillStyle = "#3498DB";
+        ctx.strokeStyle = "black";
+        ctx.font = "20px Georgia";
+        ctx.lineWidth = 10;
+        ctx.arc(h_center, k_center, 35, 0, 2 * Math.PI, false);
+        ctx.fill();
+        // escribir texto dentro del circulo
+        ctx.beginPath();
+        ctx.fillStyle = "black";
+        // str = "casa";
+        // llenar texto en cir
+        ctx.fillText(str, (h_center - str.length*4), k_center+4);
+
+      }else {
+
+        if((i % 2) === 0){
+
+          hijoDerecho(array[i],h_center +50,k_center +50);
+        }else{
+          hijoIzquierdo(array[i],h_center,k_center +50);
+        }
+      }
+  }
+  document.getElementById("txtInsert").value ="";
+/*
+  if(!n){
+    ctx.arc(h_center, k_center, 35, 0, 2 * Math.PI, false);
+    ctx.fill();
+    // escribir texto dentro del circulo
+    ctx.beginPath();
+    ctx.fillStyle = "black";
+    // str = "casa";
+    // llenar texto en cir
+    ctx.fillText(str, (h_center - str.length*4), k_center+4);
+    bool = 0;
+    n++;
+  }else {
+    if(bool === 0){
+
+      hijoIzquierdo(str, h_center, k_center);
+      bool = 1;
+      n++;
+    }else{
+      hijoDerecho(str,h_center,k_center);
+      bool = 0;
+      n++;
+    }
+
+  }
+  */
+
+}
+
+function hijoDerecho(str, h, k){
+  // graficar el circulo
+  ctx.beginPath();
+  ctx.fillStyle = "#3498DB";
+  ctx.strokeStyle = "black";
+  ctx.font = "20px Georgia";
+  ctx.lineWidth = 10;
+
+  h_center = h+100;
+  k_center = k+100;
+  ctx.arc(h_center, k_center, 35, 0, 2 * Math.PI, false);
+  ctx.fill();
+
+  // escribir texto dentro del circulo
+  ctx.beginPath();
+  ctx.fillStyle = "black";
+  var str = document.getElementById("txtInsert").value;  // para tamaño de letra <5
+
+  // str = "casa";
+  ctx.fillText(str, (h_center - str.length*4), k_center+4); // llenar texto en cir
+}
+
+function hijoIzquierdo(str, h, k){
+  // graficar el circulo
+  ctx.beginPath();
+  ctx.fillStyle = "#3498DB";
+  ctx.strokeStyle = "black";
+  ctx.font = "20px Georgia";
+  ctx.lineWidth = 10;
+
+  h_center = h-100;
+  k_center = k+100;
+  ctx.arc(h_center, k_center, 35, 0, 2 * Math.PI, false);
+  ctx.fill();
+
+  // escribir texto dentro del circulo
+  ctx.beginPath();
+  ctx.fillStyle = "black";
+  var str = document.getElementById("txtInsert").value;  // para tamaño de letra <5
+
+  // str = "casa";
+  ctx.fillText(str, (h_center - str.length*4), k_center+4); // llenar texto en cir
+}
+
+
+// ctx.beginPath();
+// ctx.arc(100, 75, 40, 0, 2* Math.PI);
+// ctx.stroke();
+
+
+/*
+ctx.arc(h_center+ 100, k_center+ 100, 35, 0, 2 * Math.PI, false);
+ctx.fill();
+*/
+
+// ctx.fill();
+
+
+
+function handleClick(event){
+
+    var str = document.getElementById("txtInsert").value;
+
+    var http = new XMLHttpRequest();
+    var url = ' http://localhost:8090/altavista/getOptions?word='+str;
+    var params = 'orem=ipsum&name=binny';
+    http.open('GET', url, true);
+
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    http.onreadystatechange = function() {
+        if(http.readyState == 4 && http.status == 200) {
+            console.log(http.responseText);
+            alert(http.responseText);
+
+        }
+    }
+    http.send(params);
+}
