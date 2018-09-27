7d2041c80ec056a0c60e1e5e6797a24cdae493b8
Jeff <jeffersonquispep@gmail.com>
2018-09-27 01:50:45 -0500

    core

--- core/main.cpp
+++ core/main.cpp
@@ -6,89 +6,28 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-
       int *word;
       int *start;
       Tree *t=new Tree();
       int a,b,c,d;
-      t->add("A",a,b);
-      t->add("NA",a,b);
-      t->add("ANA",a,b);
-      t->add("NANA",a,b);
-      t->add("ANANA",a,b);
-      t->add("BANANA",a,b);
-    //cout << "arbol " << t->graphviz()<<endl;
-    cout << t->printjson()<<endl;
 
-    //cout << "encontro: "<< t->find("A")<<endl;
-  //  cout << "encontro: "<< t->find("Z")<<endl;
-
-
-      //t->print();
-      //stringstream  str;
-      //str<<new Tree();
-      //cout << ": " <<str<<'\n';
-      //printf("ok: %i\n",&c);
-
-    /*Parse *parse = new Parse("../../files/", 0);
-    parse->processFile();
-
-    int prev, next, total;
-    double time;
-
-    cout << "... Load index done! " << endl;
-    cout << "====================================" << endl;
-    cout << "   (⌐■_■)  ** ALTAVISTA ** (⌐■_■)" << endl;
-    cout << "====================================" << endl;
-
-    while(true) {
-        string word;
-        string result;
-        prev = 0;
-        next = 0;
-        total = 0;
-        time = 0;
-        int start = 0;
-        cout << "Enter your query: ";
-        getline(cin, word);
-        vector <Result> results;
-        parse->find(word, results, total, next, prev, time, start, 30);
-
-        while (true) {
-            for (int i = 0; i < results.size(); i++) {
-                cout << "[" << i + 1 << "] " << results[i].title << " | " << results[i].preview << endl;
-                if (i > 5) {
-                    break;
-                }
-            }
-            if (results.size() > 5) {
-                cout << "..." << endl;
-            }
-            cout << endl << "About: " << total << " results (" << time << " seconds)" << endl << endl;
-
-            cout << "Do you want to open any result [n or result number]? Type 0 to go back to search  ";
-            getline(cin, result);
-            if (result == "0") {
-                break;
-            } else if (all_of(result.begin(), result.end(), ::isdigit)) {
-                int index = atoi(result.c_str());
-                if (index < 1 || index > total) {
-                    cout << "**ERROR** Invalid result number " << endl;
-                } else {
-                    int docIdInt = results[index - 1].docNumber;
-                    Document * doc = parse->getDocument(docIdInt);
-                    cout << doc->title << endl;
-                    cout << parse->getText(docIdInt, doc->start, doc->end) << endl << endl;
-                }
-
-            } else {
-                cout << "**ERROR** not a valid number" << endl;
-            }
-            cout << endl;
-        }
-
-
-    }*/
-
+      t->add("AB",a,b);
+      t->add("CA",a,b);
+      t->add("CASA",a,b);
+      t->add("CAMA",a,b);
+      t->add("CASAS",a,b);
+      //t->add("CAPITAN",a,b);
+      //t->add("CASA",a,b);
+      //t->add("CASA",a,b);
+      //t->add("CAMA",a,b);
+      //t->add("CANTO",a,b);
+      //t->add("CASADO",a,b);
+      //cout<<t->find("CAMAS")<<endl;
+      cout << t->printjson()<<endl;
+      t->erase("CA");
+      t->erase("CAMA");
+      t->erase("CASA");
+      cout <<t->graphviz() << '\n';
+      cout << t->printjson()<<endl;
     return 0;
 }
